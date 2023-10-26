40ba213a7ca8ae4691dff73c51f09578e5e61f9c
mih sve <RMS4083@yandex.ru>
2023-10-26 14:43:21 +0300

    ОгÑJunit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,22 +1,41 @@
 package ru.netology.service;
-import static org.testng.Assert.assertEquals;
+
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testCashbackBelowLimit() {
         CashbackHackService service = new CashbackHackService();
         int boundary = 900;
         int actual = service.remain(boundary);
         int expected = 100;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testCashbackAccrued() {
         CashbackHackService service = new CashbackHackService();
         int boundary = 1000;
         int actual = service.remain(boundary);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void testCashbackWhenBoundaryZero() {
+        CashbackHackService service = new CashbackHackService();
+        int boundary = 0;
+        int actual = service.remain(boundary);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void testCashbackAboveLimit() {
+        CashbackHackService service = new CashbackHackService();
+        int boundary = 1001;
+        int actual = service.remain(boundary);
+        int expected = 0;
+        assertEquals(expected, actual);
     }
 }
